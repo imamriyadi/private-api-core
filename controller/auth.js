29b6f5b60c373abfd4c54790e65603085b6b5fed
imam riyadi <imamriyadi32@gmail.com>
2020-03-02 02:06:52 +0700

    .

--- controller/auth.js
+++ controller/auth.js
@@ -39,7 +39,7 @@ class Auth {
             password: bcrypt.hashSync(password, salt)
         }).then(function (result) {
             res.json({
-                status: 'success',
+                status: 'successd',
                 message: 'Insert Data Success',
                 data: result
             })
